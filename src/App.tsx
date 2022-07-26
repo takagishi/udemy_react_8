5706e70c150c5b34f4768bfc91ffe26d65e20ce4
daichi takagishi <solar.eeclipse@gmail.com>
2022-07-27 03:32:14 +0900

    最新化

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import axios from "axios";
+import { useState } from "react";
+import { Todo } from "./Todo";
 import "./styles.css";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: " sameple",
+  hobbies: ["movie", "snowboarding"]
+};
 
 export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>();
+
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18" />
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos &&
+        todos.map((todo) => (
+          <Todo
+            title={todo.title}
+            userId={todo.userId}
+            completed={todo.completed}
+          />
+        ))}
     </div>
   );
 }
